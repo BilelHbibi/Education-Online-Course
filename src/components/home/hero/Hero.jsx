42c531ae396cc9485071d97caa9e47307e4594a9
bilel <bilelhabibi30@gmail.com>
2025-03-11 06:58:02 +0100

    first commit

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <Heading
+              subtitle="WELCOME TO ACADEMIA"
+              title="Best Online Education Expertise"
+            />
+            <p>
+              Expand your knowledge with top-tier online courses, designed to
+              empower learners from all backgrounds with expert-driven
+              education.
+            </p>
+
+            <div className="button">
+              <button className="primary-btn">
+                GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+              <button>
+                VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="margin"></div>
+    </>
+  );
+};
+
+export default Hero;
